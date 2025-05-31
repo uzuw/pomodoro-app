1e3b1dcfc1fd267ffbb2388dd52cdb678a517b17
uzuw <uzuwxd@gmail.com>
2025-05-31 23:23:01 +0545

    changes

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Timer from "@/app/components/Timer";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <Timer />
-    </main>
-  );
-}
